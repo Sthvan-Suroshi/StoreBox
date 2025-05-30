c0e63f92577c03a46a34ecd56f65ff86e6feef89
Sthvan-Suroshi <123sthavansuroshi@gmail.com>
2025-05-30 23:04:53 +0530

    feat: add db connection

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,30 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { createPool, type Pool } from "mysql2/promise";
 
 import { env } from "~/env";
 import * as schema from "./schema";
 
 /**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
+ * Cache the database connection in development. This avoids creating a new connection on every HMR update. Does not create a new connection
  */
-const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
-};
 
-export const client =
-  globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+const globalForDb = globalThis as unknown as { conn: Pool | undefined };
 
-export const db = drizzle(client, { schema });
+const conn =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 5,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForDb.conn = conn;
+}
+
+conn.addListener("error", (err) => {
+  console.error("Database connection error ", err);
+});
