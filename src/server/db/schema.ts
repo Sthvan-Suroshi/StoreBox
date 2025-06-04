67e26d2a3773d3a5d9c416647fe99914d6b798e6
Sthvan-Suroshi <123sthavansuroshi@gmail.com>
2025-06-04 22:12:03 +0530

    feat: add schema for files and folders

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,37 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  int,
+  text,
+  singlestoreTable,
+  index,
+  singlestoreTableCreator,
+} from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const createTable = singlestoreTableCreator(
+  (name) => `StoreBox_${name}`,
+);
+
+export const files = createTable(
+  "files_table",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    name: text("name"),
+    size: int("size"),
+    url: text("url"),
+    parent: int("parent").notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = createTable(
+  "folders_table",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    name: text("name").notNull(),
+    parent: int("parent"),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
